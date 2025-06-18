45c949e7545431479d7f5b50bf1c500ce87e9e32
marcobalestrin <57968309+marcobalestrin@users.noreply.github.com>
2025-06-18 18:26:33 -0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -5,13 +5,13 @@ import Parser
 import Interpreter
 import TypeChecker
 
--- main = getContents >>= print .eval . typecheck . parser . lexer
+main = getContents >>= print .eval . typecheck . parser . lexer
 
 --Mostra todas as etapas
-main = do
-  input <- getContents
-  let tokens = lexer input
-  print tokens
-  let ast = parser tokens
-  print ast
-  print (eval (typecheck ast))
+-- main = do
+--   input <- getContents
+--   let tokens = lexer input
+--   print tokens
+--   let ast = parser tokens
+--   print ast
+--   print (eval (typecheck ast))
